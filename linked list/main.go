29ee2354ac01047951a03090d5f1df56ecf37f52
Ananya E P <areduvail@gmail.com>
2024-01-07 18:21:36 +0530

    linked list insertion

---
+++ linked list/main.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+type node struct {
+	data int
+	next *node
+}
+
+type linkedList struct {
+	head   *node
+	length int
+}
+
+func (l *linkedList) prepend(n *node) {
+	second := l.head
+	l.head = n
+	l.head.next = second
+	l.length++
+}
+
+func (l linkedList) printListData() {
+	toPrint := l.head
+	for l.length != 0 {
+		fmt.Printf("%d ", toPrint.data)
+		toPrint = toPrint.next
+		l.length--
+	}
+	fmt.Printf("\n")
+}
+
+func main() {
+	mylist := linkedList{}
+	node1 := &node{data: 20}
+	node2 := &node{data: 22}
+	node3 := &node{data: 30}
+	node4 := &node{data: 11}
+	node5 := &node{data: 10}
+	node6 := &node{data: 39}
+	mylist.prepend(node1)
+	mylist.prepend(node2)
+	mylist.prepend(node3)
+	mylist.prepend(node4)
+	mylist.prepend(node5)
+	mylist.prepend(node6)
+	mylist.printListData()
+}
