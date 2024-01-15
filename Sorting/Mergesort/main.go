e167a87ccf3789b9ac7c09945dc2ba38919c20f3
Ananya E P <areduvail@gmail.com>
2024-01-15 10:13:15 +0530

    added merge sorting

---
+++ Sorting/Mergesort/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+)
+
+func mergeSort(arr []int) []int {
+	if len(arr) <= 1 {
+		return arr
+	}
+
+	mid := len(arr) / 2
+	left := mergeSort(arr[:mid])
+	right := mergeSort(arr[mid:])
+
+	return merge(left, right)
+}
+
+func merge(left, right []int) []int {
+	result := make([]int, 0, len(left)+len(right))
+	i, j := 0, 0
+
+	for i < len(left) && j < len(right) {
+		if left[i] <= right[j] {
+			result = append(result, left[i])
+			i++
+		} else {
+			result = append(result, right[j])
+			j++
+		}
+	}
+
+	result = append(result, left[i:]...)
+	result = append(result, right[j:]...)
+
+	return result
+}
+
+func main() {
+	data := []int{38, 27, 43, 3, 9, 82, 10}
+
+	fmt.Println("Unsorted Array:", data)
+
+	sortedArray := mergeSort(data)
+
+	fmt.Println("Sorted Array:", sortedArray)
+}
