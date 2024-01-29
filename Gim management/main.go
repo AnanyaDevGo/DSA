c0ef5e24bdb902d44ebfebb1c49150339b3a7145
Ananya E P <areduvail@gmail.com>
2024-01-29 10:21:29 +0530

    updated code

---
+++ Gim management/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+type Node struct {
+	ID    int
+	Name  string
+	Left  *Node
+	Right *Node
+}
+
+func NewNode(id int, name string) *Node {
+	return &Node{ID: id, Name: name, Left: nil, Right: nil}
+}
+
+func InorderTraversal(node *Node) {
+	if node != nil {
+		InorderTraversal(node.Left)
+		fmt.Printf("ID:%d, Name: %s\n", node.ID, node.Name)
+		InorderTraversal((node.Right))
+	}
+}
+
+func main() {
+	root := NewNode(1, "Anu")
+	root.Left = NewNode(2, "Bibi")
+	root.Right = NewNode(3, "Charls")
+	root.Left.Left = NewNode(4, "Devu")
+	root.Left.Right = NewNode(5, "Edwin")
+
+	fmt.Println("Result(ID and Name) ")
+	InorderTraversal(root)
+}
