bce2bf030385b6d5858141192a29dacc2b5721a6
Ananya E P <areduvail@gmail.com>
2024-02-05 11:55:17 +0530

    updated

--- sample/sample.go
+++ sample/sample.go
@@ -24,13 +24,13 @@ func countDuplicates(arr1, arr2 []int) int {
 	// }
 
 
-// func reverseString(s string) string {
-// 	runes := []rune(s)
-// 	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
-// 		runes[i], runes[j] = runes[j], runes[i]
-// 	}
-// 	return string(runes)
-// }
+func reverseString(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
 
 // 	func main() {
 // 		str := "hello"
